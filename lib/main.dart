fb8a85d93cfab119f3dd799efc27a424cb71726d
Rofayda <rofaydabassem@gmail.com>
2024-03-04 00:25:52 +0200

    Initial project files

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:demo/screens/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashAnimated());
+  }
+}
